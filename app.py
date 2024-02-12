31a78958131d476ddc0b60c01f5a701908b9b697
EneasJr-Rodrigues <eneas.rodrigues25@gmail.com>
2024-02-12 14:03:49 -0300

    fix correcoes na arquitetura

--- app.py
+++ app.py
@@ -1,36 +1,23 @@
 # app.py
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:YourPassword123@db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-class Item(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def index():
-    items = Item.query.all()
-    return render_template('index.html', items=items)
-
-@app.route('/add', methods=['POST'])
-def add():
-    name = request.form['name']
-    new_item = Item(name=name)
-    db.session.add(new_item)
-    db.session.commit()
-    return redirect(url_for('index'))
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    item_to_delete = Item.query.get(id)
-    db.session.delete(item_to_delete)
-    db.session.commit()
-    return redirect(url_for('index'))
+def home():
+    with app.app_context():
+        users = User.query.all()
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True, host='0.0.0.0')
